e9d4e055ee2f1fd7c4336f68d737301a01880ef6
Christopher Savan <cxs7700@rit.edu>
2020-03-07 22:39:45 -0800

    user helper functions, socket handlers

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const http = require('http');
 const socketIO = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const port = process.env.PORT || 5000;
 
@@ -10,15 +12,38 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server) // Sets up new server instance of socket.io
 
-// On connection, you get a socket
-io.on("connection", socket => {
-  console.log("New client connected.");
+io.on("connection", (socket) => {
+  console.log("New user has connected.");
   
-  socket.on("incoming player move", (move) => {
+  // Join
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    
+    if (error) {
+      return callback(error);
+    }
+    
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` })
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined.`});
+    socket.join(user.room);
+    callback();
+  });
+  
+  // Send move
+  socket.on('playerTurn', (message, callback) => {
+    const user = getUser(socket.id);
+    
+    io.to(user.room).emit('message', { user: user.name, text: message});
+    
+    callback();
+  })
+  
+  // Incoming move
+  socket.on("incomingMove", (move) => {
     socket.broadcast.emit("outgoing player move", {playerMove: move});
   });
   
-  socket.on("disconnect", () => console.log("Client disconnected."));
+  socket.on("disconnect", () => console.log("User has disconnected."));
 });
 
 // Use different routing files
--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -3,67 +3,93 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 import styled from 'styled-components'
 
-// export default class TicTacToe extends React.Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       playerTurn: "X",
-//       board: ["","","","","","","","",""]
-//     }
-//   }
-const TicTacToe = () => {
-  const [board, setBoard] = useState(["","","","","","","","",""]);
-  const [playerTurn, setPlayerTurn] = useState("")
+let socket;
+
+// `location` comes from react-router and gives a location prop
+const TicTacToe = ({ location }) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const [playerTurn, setPlayerTurn] = useState("X")
+  const [gameBoard, setGameBoard] = useState(["","","","","","","","",""]);
+  const endpoint = 'localhost:5000' // backend port
+  
+  useEffect(() => {
+    // Get name and room from URL
+    // Parses query string from URL
+    const { name, room } = queryString.parse(location.search)
+    socket = io(endpoint);
+    setName(name)
+    setRoom(room)
+    socket.emit('join', { name, room }, () => {
+      
+    });
+    
+    return () => {
+      socket.emit('disconnect');
+      
+      socket.off();
+    }
+  }, [endpoint, location.search]);
+  
+  useEffect(() => {
+    socket.on('sendMove', (playerTurn) => {
+      
+    })
+  })
+  
+  // Send move
+  const sendMove = (index) => {
+    playerMove(index)
+  }
+  
+  const playerMove = (index) => {
+    let turn = playerTurn;
+    let board = gameBoard;
+    let winningMoves = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    
+    if (board[index] === '') {
+      board[index] = turn
+      for (let i = 0; i < winningMoves.length; i++) {
+        let winningRow = winningMoves[i]
+        let p1 = winningRow[0]
+        let p2 = winningRow[1]
+        let p3 = winningRow[2]
+        if (board[p1] !== '' && board[p1] === board[p2] && board[p2] === board[p3] && board[p3] === board[p1]) {
+          alert(`${turn} is the winner!`)
+          board = ["","","","","","","","",""]
+        }
+      }
+      turn = turn === "X" ? "O" : "X"
+      setPlayerTurn(turn)
+      setGameBoard(board)
+    } else {
+      alert("Choose a different spot.")
+    }
+  }
   
   return (
     <Container>
-      <Board>
+      <GameBoard>
         {
-          board.map((square,index) => {
-            return <Square onClick={() => playerMove(index,playerTurn,board)}>{square}</Square>
+          gameBoard.map((square,index) => {
+            return <Square onClick={() => playerMove(index)}>{square}</Square>
           })
         }
-      </Board>
+      </GameBoard>
     </Container>
   )
 }
 
-function playerMove(index,playerTurn,gameBoard) {
-  let turn = playerTurn;
-  let board = gameBoard
-  let winningMoves = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-  ]
-  
-  if (board[index] === '') {
-    board[index] = turn
-    for (let i = 0; i < winningMoves.length; i++) {
-      let winningRow = winningMoves[i]
-      let p1 = winningRow[0]
-      let p2 = winningRow[1]
-      let p3 = winningRow[2]
-      if (board[p1] !== '' && board[p1] === board[p2] && board[p2] === board[p3] && board[p3] === board[p1]) {
-        alert(`${turn} is the winner!`)
-        board = ["","","","","","","","",""]
-      }
-    }
-    turn = turn === "X" ? "O" : "X"
-    this.setBoard({
-      board: board
-    })
-  } else {
-    alert("Choose a different spot.")
-  }
-}
-
-const Board = styled.div`
+const GameBoard = styled.div`
   display: flex;
   flex-flow: row wrap;
   width: 390px;
